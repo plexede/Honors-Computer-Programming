35eda4bdd93a45b277ae3ec423caf477802da88d
okm <64970074+plexede@users.noreply.github.com>
2025-11-18 23:14:48 -0500

    one and two

--- Java 6/oneAndTwo.java
+++ Java 6/oneAndTwo.java
@@ -1,17 +1,27 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class oneAndTwo {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a number between 1 and 5: ");
-        int number = scanner.nextInt();
-        if (number > 5) // 1
+        int userInput = scanner.nextInt();
+        if (userInput > 5) // 1
         { 
         System.out.println("Number is too large!");
-        } else if (number < 1) { // 2
+        } else if (userInput < 1) { // 2
             System.out.println("Number is too small!");
         } else { // 3
             System.out.println("The number is just right!");
+            partTwo(userInput);
+        }
+    }
+    static public void partTwo(int userInput) {
+        Random r = new Random();
+        if (userInput == r.nextInt(1, 5)) {
+            System.out.println("And you guessed the magic number!");
+        } else {
+            System.out.println("Sorry, your guess doesn't match the magic number.");
         }
     }
 }

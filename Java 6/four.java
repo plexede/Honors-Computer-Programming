1951d2d9b5d7f9ae15f71620ef55869e273f8e10
okm <64970074+plexede@users.noreply.github.com>
2025-11-21 18:50:03 -0500

    answer processing convention

--- Java 6/four.java
+++ Java 6/four.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class four {
     static Scanner s = new Scanner(System.in);
+    // i have no idea what the convention is for this language
     static String userInput;
     static String optionA;
     static String optionB;
@@ -9,22 +10,34 @@ public class four {
     public static void main(String[] args) {
         // System.out.println("You entered: " + optionsAB("hello", "hi"));
         int storyIndex = 0;
-        switch (storyIndex) {
-            case 0:
-                System.out.println("This is the beginning of the story. Are you ready?" +
-                        "\n yes \n no");
-                optionsAB(optionA, optionB);
-                break;
-            case 1:
-                System.out.println("This is the middle of the story.");
-                break;
+        int storyEnd = 100;
+        while (storyIndex != 100) {
+            if (storyIndex > storyEnd) {
+                System.out.println("!! Please increase storyEnd value !!");
+            }
+            ;
+            switch (storyIndex) {
+                case 0:
+                    System.out.println("This is the beginning of the story. Are you ready?");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 1:
+                    System.out.println("This is the middle of the story.");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+            }
         }
     }
 
-    public static String optionsAB(String optionA, String optionB) {
+    public static boolean optionsAB(String optionA, String optionB) {
+        System.out.println(optionA + "\n" + optionB);
         do {
             userInput = s.nextLine();
         } while (!(userInput.contentEquals(optionA) || userInput.contentEquals(optionB)));
-        return userInput;
+        return userInput.contentEquals(optionA);
     }
 }

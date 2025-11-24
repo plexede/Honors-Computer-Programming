b09b79715432ec69d217b65e69ebaeb62b1f1704
plexede <64970074+plexede@users.noreply.github.com>
2025-11-24 09:10:36 -0500

    full story outline

--- Java 6/four.java
+++ Java 6/four.java
@@ -10,8 +10,8 @@ public class four {
     public static void main(String[] args) {
         // System.out.println("You entered: " + optionsAB("hello", "hi"));
         int storyIndex = 0;
-        int storyEnd = 100;
-        while (storyIndex != 100) {
+        int storyEnd = 11;
+        while (storyIndex != storyEnd) {
             if (storyIndex > storyEnd) {
                 System.out.println("!! Please increase storyEnd value !!");
             }
@@ -27,7 +27,59 @@ public class four {
                     System.out.println("This is the middle of the story.");
                     optionA = "yes";
                     optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 2 : storyEnd;
+                    break;
+                case 2:
+                    System.out.println("Another part of the story.");
+                    optionA = "yes";
+                    optionB = "no";
                     storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 3:
+                    System.out.println("Another part of the story.");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 4:
+                    System.out.println("Another part of the story.");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 5:
+                    System.out.println("Another part of the story.");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 6:
+                    System.out.println("Another part of the story.");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 7:
+                    System.out.println("Another part of the story.");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 8:
+                    System.out.println("Another part of the story.");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 9:
+                    System.out.println("Another part of the story.");
+                    optionA = "yes";
+                    optionB = "no";
+                    storyIndex = optionsAB(optionA, optionB) ? 1 : storyEnd;
+                    break;
+                case 10:
+                    System.out.println("The End!");
+                    storyIndex = storyEnd;
                     break;
             }
         }
@@ -37,7 +89,7 @@ public class four {
         System.out.println(optionA + "\n" + optionB);
         do {
             userInput = s.nextLine();
-        } while (!(userInput.contentEquals(optionA) || userInput.contentEquals(optionB)));
-        return userInput.contentEquals(optionA);
+        } while (!(userInput.equalsIgnoreCase(optionA) || userInput.equalsIgnoreCase(optionB)));
+        return userInput.equalsIgnoreCase(optionA);
     }
 }

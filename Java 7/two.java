78243b6b60de9d7ee1804c6086ff8f99c0348eac
plexede <64970074+plexede@users.noreply.github.com>
2025-12-10 10:19:48 -0500

    slight progress idk im hungry

--- Java 7/two.java
+++ Java 7/two.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class two {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
-        for (int i = 0; i < 11; i++) {
-            String userInput = s.nextLine();
+        String userInput = s.nextLine();
+        for (int i = 0; i < userInput.length(); i++) {
+            System.out.println(userInput.charAt(i));
         }
     }
 }

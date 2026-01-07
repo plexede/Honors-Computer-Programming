3062b2bf735b69f3d63b0efcb4e59311583180f4
okm <64970074+plexede@users.noreply.github.com>
2026-01-06 21:57:31 -0500

    finished two

--- Java 7/two.java
+++ Java 7/two.java
@@ -7,5 +7,29 @@ public class two {
         for (int i = 0; i < userInput.length(); i++) {
             System.out.println(userInput.charAt(i));
         }
+        // check if palindrome
+        String stringBackwards = "";
+        for (int i = userInput.length() - 1; i >= 0; i--) {
+            stringBackwards = stringBackwards + userInput.charAt(i);
+        }
+        System.out.println(stringBackwards);
+        if (userInput.contentEquals(stringBackwards)) {
+            System.out.println("Palindrome!");
+        } else {
+            System.out.println("Not a palindrome!");
+        }
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < alphabet.length(); i++) {
+            int instances = 0;
+            for (int j = 0; j < userInput.length(); j++) {
+                if (userInput.toLowerCase().charAt(j) == alphabet.toLowerCase().charAt(i)) {
+                    instances++;
+                }
+            }
+            if (instances > 0) {
+                System.out.println("There are " + instances + " instances of " + alphabet.charAt(i));
+            }
+        }
     }
 }

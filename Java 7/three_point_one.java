a213250128d69ab27f830645f00f382b10d67bcf
plex <64970074+plexede@users.noreply.github.com>
2026-01-12 08:18:22 -0500

    progress

--- Java 7/three_point_one.java
+++ Java 7/three_point_one.java
@@ -8,12 +8,13 @@ public class three_point_one {
         int c;
         boolean isTriangle;
         do {
+            System.out.println("Enter the sides of a valid triangle.");
             System.out.println("Side a: ");
             a = s.nextInt();
             System.out.println("Side b: ");
-            int b = s.nextInt();
+            b = s.nextInt();
             System.out.println("Side c: ");
-            int c = s.nextInt();
+            c = s.nextInt();
             isTriangle = ((a + b > c) && (a + c > b) && (b + c > a));
         } while (!isTriangle);
         s.close();

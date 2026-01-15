640a20f273c1c627c9ff38dfd3801eb5a21d8a2b
plex <64970074+plexede@users.noreply.github.com>
2026-01-15 10:18:22 -0500

    prolly important logic fixes

--- Java 7/four.java
+++ Java 7/four.java
@@ -12,21 +12,22 @@ public class four {
         // }
 
         String slot1 = "*";
-        String slot2 = " ";
+        String slot2 = "|";
+        String slot3 = "----";
 
         for (int medianBar = max; medianBar >= 0; medianBar--) {
             printSlots(slot1, slot2, medianBar);
-            System.out.print("\t");
+            System.out.print(slot3);
             printSlots(slot2, slot1, medianBar);
             System.out.println();
         }
     }
 
     static void printSlots(String slot1, String slot2, int medianBar) {
-        for (int i = 0; i <= medianBar; i++) {
+        for (int i = 0; i < medianBar; i++) {
             System.out.print(slot1);
         }
-        for (int i = 0; i <= max - medianBar; i++) {
+        for (int i = 0; i < max - medianBar; i++) {
             System.out.print(slot2);
         }
     }

392d4b184d7a756e902b432981241b5d0fb2a48c
plex <64970074+plexede@users.noreply.github.com>
2026-01-14 19:35:44 -0500

    why are you like this

--- Java 7/four.java
+++ Java 7/four.java
@@ -1,26 +1,33 @@
 public class four {
+    static int max = 10;
+
     public static void main(String[] args) {
-        int max = 10;
-        for (int i = 0; i <= max; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
-            }
+
+        // first waterfall
+        // for (int i = 0; i <= max; i++) {
+        // for (int j = 0; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        String slot1 = "*";
+        String slot2 = " ";
+
+        for (int medianBar = max; medianBar >= 0; medianBar--) {
+            printSlots(slot1, slot2, medianBar);
+            System.out.print("\t");
+            printSlots(slot2, slot1, medianBar);
             System.out.println();
         }
+    }
 
-        for (int i = 0; i <= max; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
-
-                System.out.print("\t");
-                int spaces = i - j; // distance between
-                for (int k = 0; k <= spaces; k++) {
-                    System.out.print(" ");
-                }
-                System.out.print("*");
-
-            }
-            System.out.println();
+    static void printSlots(String slot1, String slot2, int medianBar) {
+        for (int i = 0; i <= medianBar; i++) {
+            System.out.print(slot1);
+        }
+        for (int i = 0; i <= max - medianBar; i++) {
+            System.out.print(slot2);
         }
     }
 }

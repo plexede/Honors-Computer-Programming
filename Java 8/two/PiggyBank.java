05ee537e9dee0cc4abdc550844f98e9c2f4bdb4c
okm <64970074+plexede@users.noreply.github.com>
2026-01-15 20:35:46 -0500

    format

--- Java 8/two/PiggyBank.java
+++ Java 8/two/PiggyBank.java
@@ -1,23 +1,26 @@
+import java.text.DecimalFormat;
+
 public class PiggyBank {
+    DecimalFormat fmt = new DecimalFormat("0.##");
     double balance = 0;
 
     void addPenny() {
         balance += .01;
-        System.out.println("Current Balance: " + balance);
+        System.out.println("Current Balance: " + fmt.format(balance));
     }
 
     void addNickel() {
         balance += .06;
-        System.out.println("Current Balance: " + balance);
+        System.out.println("Current Balance: " + fmt.format(balance));
     }
 
     void addDime() {
         balance += .10;
-        System.out.println("Current Balance: " + balance);
+        System.out.println("Current Balance: " + fmt.format(balance));
     }
 
     void addQuarter() {
         balance += .25;
-        System.out.println("Current Balance: " + balance);
+        System.out.println("Current Balance: " + fmt.format(balance));
     }
 }

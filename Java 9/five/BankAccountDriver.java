bd955d41355b62314be2dc657b9a0d10597f5675
plex <64970074+plexede@users.noreply.github.com>
2026-02-02 10:13:26 -0500

    sync

--- Java 9/five/BankAccountDriver.java
+++ Java 9/five/BankAccountDriver.java
@@ -3,11 +3,24 @@ import java.util.Scanner;
 public class BankAccountDriver {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int NUMBER_OF_OPTIONS = 3;
         for (boolean valid = false; !valid;) {
             int userChoice = s.nextInt();
-            valid = (userChoice > 0) && (userChoice <= 3);
+            valid = (userChoice > 0) && (userChoice <= NUMBER_OF_OPTIONS);
             if (!valid)
                 System.out.println("Invalid input!");
+        }
+        switch (userChoice) {
+            case 1:
+                // deposit
+                break;
+            case 2:
+                // withdraw
+                break;
+            case 3:
+        
+            default:
+                break;
         }
         s.close();
     }

5e4ae089bdd41980c677714987c1d40e87064865
plex <64970074+plexede@users.noreply.github.com>
2026-02-18 13:35:39 -0500

    sync

--- Java 10/two/Creature.java
+++ Java 10/two/Creature.java
@@ -6,4 +6,23 @@ public class Creature {
         this.health = health;
         this.tag = tag;
     }
+
+    public int isHit()
+    {
+        if(health <= 0)
+            return -1;
+        health -= 1;
+        return health;
+    }
+
+    public String toString()
+    {
+        return "Creature tag: " + tag +
+        "\nCreature health: " + health;
+    }
+    public void collidesWith(Creature otherCreature) {
+        if (!otherCreature.tag.contentEquals(tag))
+            isHit();
+    }
+
 }
